3336a1f29df9753ff0656ff81ab0c69d22ec2965
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-23 03:18:26 +0200

    build: added generation of javadoc and Jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,6 +128,8 @@ protobuf {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
 }
 
 publishing {
