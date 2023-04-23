385f6fe119847f35a8d63c74257baf2e6c605df2
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-23 02:18:43 +0200

    build: setting configuration for build

---
+++ build.gradle.kts
@@ -0,0 +1,192 @@
+import com.google.protobuf.gradle.id
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION as KOTLIN_VERSION
+
+plugins {
+    id("java-library")
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.qa)
+    alias(libs.plugins.publishOnCentral)
+    alias(libs.plugins.multiJvmTesting)
+    signing
+    `maven-publish`
+    alias(libs.plugins.taskTree)
+    alias(libs.plugins.protobuf)
+}
+
+group = "io.github.andreabrighi"
+class ProjectInfo {
+    val projectId = "$group.$name"
+    val fullName = "RetrofitProtobufJsonConverter"
+    val projectDetails = "Factory for retrofit that can convert a Json request into a Protobuf class"
+
+    val websiteUrl = "https://github.com/AndreaBrighi/RetrofitProtobufJsonConverter"
+    val vcsUrl = "https://github.com/AndreaBrighi/RetrofitProtobufJsonConverter.git"
+    // val scm = "scm:git:$websiteUrl.git"
+    val tags = listOf("Retrofit", "Json", "Protobuf")
+}
+
+val info = ProjectInfo()
+
+gitSemVer {
+    maxVersionLength.set(20)
+    buildMetadataSeparator.set("-")
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+multiJvm {
+    maximumSupportedJvmVersion.set(latestJavaSupportedByGradle)
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+multiJvm {
+    maximumSupportedJvmVersion.set(latestJavaSupportedByGradle)
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation(libs.bundles.protobuf)
+    implementation(libs.bundles.grpc)
+    implementation(kotlin("reflect"))
+    testImplementation(gradleTestKit())
+    testImplementation(libs.bundles.kotlin.testing)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin")) {
+            useVersion(KOTLIN_VERSION)
+            because("All Kotlin modules should use the same version, and compiler uses $KOTLIN_VERSION")
+        }
+    }
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            showCauses = true
+            showStackTraces = true
+            showStandardStreams = true
+            events(*TestLogEvent.values())
+        }
+    }
+    withType<KotlinCompile> {
+        kotlinOptions {
+            allWarningsAsErrors = true
+            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn", "-Xinline-classes")
+        }
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/java")
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        // The artifact spec for the Protobuf Compiler
+        artifact = "com.google.protobuf:protoc:3.19.4"
+    }
+    plugins {
+        // Optional: an artifact spec for a protoc plugin, with "grpc" as
+        // the identifier, which can be referred to in the "plugins"
+        // container of the "generateProtoTasks" closure.
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.45.1"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach() {
+            // The "plugins" container is a NamedDomainObjectContainer<GenerateProtoTask.PluginOptions>
+            // which can be used to configure the plugins applied to the task.
+            it.builtins {
+                id("kotlin") {
+                }
+            }
+        }
+    }
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            // Pass the pwd via -PmavenCentralPwd='yourPassword'
+            val mavenCentralPwd: String? by project
+            credentials {
+                username = "andreabrighi"
+                password = mavenCentralPwd
+            }
+        }
+        publications {
+            val converter by creating(MavenPublication::class) {
+                from(components["java"])
+                // If the gradle-publish-plugins plugin is applied, these are pre-configured
+                // artifact(javadocJar)
+                // artifact(sourceJar)
+                pom {
+                    name.set(info.fullName)
+                    description.set(info.projectDetails)
+                    url.set(
+                        info.websiteUrl
+                    )
+                    licenses {
+                        license {
+                            name.set("MIT")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Andrea Brighi")
+                        }
+                    }
+                    scm {
+                        url.set(
+                            info.vcsUrl
+                        )
+                        connection.set(
+                            info.vcsUrl
+                        )
+                    }
+                }
+            }
+            signing { sign(converter) }
+        }
+    }
+}
+
+if (System.getenv("CI") == "true") {
+    signing {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+} else {
+    signing {
+        useGpgCmd()
+        sign(configurations.archives.get())
+    }
+}
